f5a4c85dd786ffeeb64b707aceb377ed53c0bc8c
Koen Hagen <koen@hagen-dewit.nl>
2021-04-12 15:14:37 +0200

    fix

--- roulette.py
+++ roulette.py
@@ -4,6 +4,7 @@ from api import api_call
 
 async def random_embed():
     film_list = await api_call('list/Lb60/entries')
-    film = film_list['items'][randrange(len(film_list)['items'])]
+    random_int = randrange(len(film_list)['items'])
+    film = film_list['items'][random_int]
 
     return await film_embed(film['film']['name'])
