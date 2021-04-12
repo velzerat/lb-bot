7bc62c234168b7eae4f9a8130d6070ade97f6803
Koen Hagen <koen@hagen-dewit.nl>
2021-04-12 15:51:37 +0200

    It works

--- roulette.py
+++ roulette.py
@@ -3,8 +3,18 @@ from film import film_embed
 from api import api_call
 
 async def random_embed():
-    film_list = await api_call('list/Lb60/entries')
-    random_int = randrange(film_list['metadata']['totalFilmCount'])
-    film = film_list['items'][random_int]
 
-    return await film_embed(film['film']['name'])
+    with open("films.txt") as f:
+        film = random_line(f)
+
+    return await film_embed(film)
+
+def random_line(afile, default=None):
+    line = default
+    for i, aline in enumerate(afile, start=1):
+        if randrange(i) == 0:  # random int [0..i)
+            line = aline
+    return line
+
+
+print(line)
